8dfbabc7d0418e433c5e52abde583c0e7851bf6a
RobinWiese <95370225+RobinWiese@users.noreply.github.com>
2021-12-01 17:31:30 +0100

    Create Wait.java

---
+++ Wait.java
@@ -0,0 +1,20 @@
+package Util;
+
+public class Wait {
+
+    public static void wait(int ms){
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Wait();
+    }
+
+}
